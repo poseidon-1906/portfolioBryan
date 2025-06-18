bc9b17616ab0eeacfdf13cd35d0e11bab07bba7c
TIYOU YOUMBI miguel bryan <157957213+poseidon-1906@users.noreply.github.com>
2025-06-18 09:02:51 +0100

    first comit

---
+++ js/script.js
@@ -0,0 +1,168 @@
+// htmlcss progress circular bar 
+let htmlProgress = document.querySelector(".html-css"),
+  htmlValue = document.querySelector(".html-progress");
+
+let htmlStartValue = 0,
+  htmlEndValue = 90,
+  htmlspeed = 30;
+
+let progresshtml = setInterval(() => {
+  htmlStartValue++;
+
+  htmlValue.textContent = `${htmlStartValue}%`;
+  htmlProgress.style.background = `conic-gradient(#fca61f ${
+    htmlStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (htmlStartValue == htmlEndValue) {
+    clearInterval(progresshtml);
+  }
+}, htmlspeed);
+
+// javasript progress circular bar 
+let javascriptProgress = document.querySelector(".javascript"),
+  javascriptValue = document.querySelector(".javascript-progress");
+
+let javascriptStartValue = 0,
+  javascriptEndValue = 65,
+  jsspeed = 30;
+
+let progressjs = setInterval(() => {
+  javascriptStartValue++;
+
+  javascriptValue.textContent = `${javascriptStartValue}%`;
+  javascriptProgress.style.background = `conic-gradient(#7d2ae8 ${
+    javascriptStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (javascriptStartValue == javascriptEndValue) {
+    clearInterval(progressjs);
+  }
+}, jsspeed);
+
+// sql progress circular bar 
+let sqlProgress = document.querySelector(".sql"),
+  sqlValue = document.querySelector(".sql-progress");
+
+let sqlStartValue = 0,
+    sqlEndValue = 80,
+    sqlspeed = 30;
+
+let progresssql = setInterval(() => {
+  sqlStartValue++;
+
+  sqlValue.textContent = `${sqlStartValue}%`;
+  sqlProgress.style.background = `conic-gradient(#20c997 ${
+    sqlStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (sqlStartValue == sqlEndValue) {
+    clearInterval(progresssql);
+  }
+}, sqlspeed);
+
+// reactjs progress circular bar 
+let reactProgress = document.querySelector(".reactjs"),
+  reactValue = document.querySelector(".reactjs-progress");
+
+let reactStartValue = 0,
+  reactEndValue = 30,
+  rjsspeed = 30;
+
+let progressreact = setInterval(() => {
+  reactStartValue++;
+
+  reactValue.textContent = `${reactStartValue}%`;
+  reactProgress.style.background = `conic-gradient(#3f396d ${
+    reactStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (reactStartValue == reactEndValue) {
+    clearInterval(progressreact);
+  }
+}, rjsspeed);
+
+
+// filter using javascript
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post").show("1000");
+    } else {
+      $(".post")
+        .not("." + value)
+        .hide("1000");
+      $(".post")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+});
+
+
+// Créer une barre de navigation fixe
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar-top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar-top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+
+// ajouter des fonctionalitées sur les boutons 
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click",function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+
+//=========================== Email JS===========================
+
+
+function envoiMail(){
+  var parmsP = {
+    noms : document.getElementById("exampleFormControlInput1").value,
+    email : document.getElementById("exampleFormControlInput2").value,
+    message : document.getElementById("exampleFormControlTextarea1").value,
+ };
+
+ emailjs.send('service_xbevvz7', 'template_qo5iqt5', parmsP).then(
+    (response) => {
+      alert("Votre mail à bien été envoyé !");
+         // --- Ajout pour vider les champs ---
+      document.getElementById("exampleFormControlInput1").value = ''; // Vide le champ Nom
+      document.getElementById("exampleFormControlInput2").value = ''; // Vide le champ Email
+      document.getElementById("exampleFormControlTextarea1").value = ''; // Vide le champ Message
+    },
+      (error) => {
+      alert("ERREUR !");
+      },)
+};
